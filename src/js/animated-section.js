1bae8c472e0a072f792eaeb2cfa63f87fd0cb822
Daria Golovashova <your_email>
2025-02-08 11:50:31 +0100

    Markup added to the covers section (HTML/CSS), images with JPEG extension removed for this section, and replaced with images in WebP format

---
+++ src/js/animated-section.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const gallery = document.querySelector(".covers-gallery");
+
+//     const observer = new IntersectionObserver((entries) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 gallery.classList.add("animate");
+//             }
+//         });
+//     }, { threshold: 0.2 });
+
+//     observer.observe(gallery);
+// });
