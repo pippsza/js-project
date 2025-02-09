7cef3fbfc83bb7d719bf3076ab5a4be253ac7d2e
Tetiana <ystritsam@gmail.com>
2025-02-09 17:33:23 +0100

    finished section footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,8 +1,13 @@
 import iziToast from 'izitoast';
 
-let emailInput = document.getElementById('user-email');
-let errorMessage = document.getElementById('footer-error');
-let successMessage = document.getElementById('footer-success');
+const emailInput = document.getElementById('user-email');
+const commentInput = document.getElementById('user-comments');
+const errorMessage = document.getElementById('footer-error');
+const successMessage = document.getElementById('footer-success');
+const overlay = document.querySelector('.overlay');
+const modalWindow = document.getElementById('modal-footer');
+const sendButton = document.querySelector('.footer-button');
+const form = document.querySelector('.footer-form');
 
 //Перевірка чи введено валідний email, виділення кольором і підпис
 emailInput.addEventListener('input', function () {
@@ -23,16 +28,71 @@ emailInput.addEventListener('input', function () {
     emailInput.classList.remove('valid', 'invalid');
   }
 });
+
+//Відправка форми
+sendButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  const email = emailInput.value.trim();
+  const comment = commentInput.value.trim();
+
+  if (!email || !comment) {
+    iziToast.info({
+      title: 'Info',
+      message: 'Please fill in all fields.',
+      position: 'center',
+      timeout: 5000,
+    });
+    return;
+  }
+  fetch('https://portfolio-js.b.goit.study/api/requests', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, comment }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.title && data.message) {
+        document.getElementById('modal-title').textContent = data.title;
+        document.getElementById('modal-message').textContent = data.message;
+        modalWindow.classList.add('show');
+        overlay.classList.add('show');
+
+        //Скидання форми
+        form.reset();
+        emailInput.classList.remove('valid', 'invalid');
+        successMessage.style.display = 'none';
+        errorMessage.style.display = 'none';
+      } else {
+        iziToast.error({
+          title: 'Error',
+          message: 'Invalid e-mail. Check the data you entered.',
+          position: 'center',
+          timeout: 5000,
+        });
+      }
+    })
+    .catch(error => {
+      //Обробка помилки під час запиту
+      iziToast.error({
+        title: 'Network Error',
+        message:
+          'Unable to connect to the server. Please check your internet connection.',
+        position: 'center',
+        timeout: 5000,
+      });
+    });
+});
 //Закриття модального вікна
 document.addEventListener('DOMContentLoaded', function () {
-  const modalWindow = document.getElementById('modal-footer');
   const closeButtonModal = document.querySelector('.close-modal');
 
   function closeWindow() {
-    modalWindow.style.display = 'none';
+    modalWindow.classList.remove('show');
+    overlay.classList.remove('show');
   }
   closeButtonModal.addEventListener('click', closeWindow);
-
   modalWindow.addEventListener('click', function (event) {
     if (event.target === modalWindow) {
       closeWindow();
@@ -44,68 +104,3 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
-//Відправка форми
-document
-  .querySelector('.footer-button')
-  .addEventListener('click', function (event) {
-    event.preventDefault();
-    const email = emailInput.value;
-    const comment = document.getElementById('user-comments').value;
-
-    if (!email || !comment) {
-      iziToast.info({
-        title: 'Info',
-        message: 'Check the data you entered.',
-        position: 'center',
-        timeout: 5000,
-      });
-      return;
-    }
-    fetch('https://portfolio-js.b.goit.study/api/requests', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, comment }),
-    })
-      .then(response => {
-        console.log('Response:', response);
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Server response:', data);
-        if (data.title && data.message) {
-          console.log('Showing modal window');
-          document.getElementById('modal-title').textContent = data.title;
-          document.getElementById('modal-message').textContent = data.message;
-          document.getElementById('modal-footer').classList.add('show');
-          //Сброс формы
-          document.querySelector('.footer-form').reset();
-          emailInput.classList.remove('valid', 'invalid');
-          successMessage.style.display = 'none';
-          errorMessage.style.display = 'none';
-        } else {
-          iziToast.error({
-            title: 'Server Error',
-            message:
-              'There was an error processing your request. Please try again.',
-            position: 'center',
-            timeout: 5000,
-          });
-        }
-      })
-      .catch(error => {
-        //Обробка помилки під час запиту
-        iziToast.error({
-          title: 'Network Error',
-          message:
-            'Unable to connect to the server. Please check your internet connection.',
-          position: 'center',
-          timeout: 5000,
-        });
-        console.error('Fetch error:', error);
-      });
-  });
