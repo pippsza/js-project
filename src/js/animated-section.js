ac7e8d90512642f1852ca5c236a6cc030fcd6a69
Daria Golovashova <your_email>
2025-02-09 19:27:49 +0100

    fixed all mistakes

--- src/js/animated-section.js
+++ src/js/animated-section.js
@@ -1,16 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const gallery = document.querySelector(".covers-gallery");
-// const rows = document.querySelectorAll(".covers-list");
-// rows.forEach(row => {
-//     let shuffledImages = [...gallery, gallery[Math.floor(Math.random() * gallery.length)]]; // Додаємо випадкове повторення
-//     shuffledImages = shuffledImages.sort(() => Math.random() - 0.5); 
-// shuffledImages.forEach(img => {
-//       const li = document.createElement("li");
-//       li.classList.add("covers-item");
-//       li.innerHTML = <img src="${img}" />;
-//       row.appendChild(li);
-//     });
-//   });
-// });
+
 
     
