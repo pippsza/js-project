e4360a25000172573f86e1be5bb17d94d35c88c3
pippsza <bastahov18@gmail.com>
2025-02-10 13:32:18 +0200

    js clog delete

--- src/js/animated-section.js
+++ src/js/animated-section.js
@@ -1,35 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const coversSection = document.querySelector('.covers-content'); 
-    const coversList = document.querySelector('.covers-list'); 
+document.addEventListener('DOMContentLoaded', function () {
+  const coversSection = document.querySelector('.covers-content');
+  const coversList = document.querySelector('.covers-list');
 
-    function isElementInViewport(el) {
-        if (!el) return false;
-        const rect = el.getBoundingClientRect();
-        console.log("Координати секції:", rect);
-    console.log("Висота вікна браузера:", window.innerHeight);
-    
+  function isElementInViewport(el) {
+    if (!el) return false;
+    const rect = el.getBoundingClientRect();
+    // console.log('Координати секції:', rect);
+    // console.log("Висота вікна браузера:", window.innerHeight);
+
     const isVisible = rect.top < window.innerHeight && rect.bottom > 0;
-    console.log("Результат перевірки (чи у viewport?):", isVisible);
-    
+    // console.log("Результат перевірки (чи у viewport?):", isVisible);
+
     return isVisible;
-       
-    }
+  }
 
-     function checkScroll() {
-        if (isElementInViewport(coversSection)) {
-            if (!coversList.classList.contains('.covers-list')) {
-                coversList.classList.add('.covers-list');
-                console.log("cover у viewport анімація не запущена");
-            }
-           
-        } else {
-            if (coversList.classList.contains('.covers-list')) {
-                coversList.classList.remove('.covers-list');
-                console.log("cover у viewport анімація запущена");
-            }
-        }
+  function checkScroll() {
+    if (isElementInViewport(coversSection)) {
+      if (!coversList.classList.contains('.covers-list')) {
+        coversList.classList.add('.covers-list');
+        // console.log("cover у viewport анімація не запущена");
+      }
+    } else {
+      if (coversList.classList.contains('.covers-list')) {
+        coversList.classList.remove('.covers-list');
+        // console.log("cover у viewport анімація запущена");
+      }
     }
+  }
 
-    checkScroll();
-    window.addEventListener('coversAnimation', checkScroll);
+  checkScroll();
+  window.addEventListener('coversAnimation', checkScroll);
 });
